8597351d241ffa1b1e1dad11ae4c0aa718480524
just-hoshinon <mwadman2018@gmail.com>
2023-05-12 20:26:05 +0300

    documentation: added swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 import { ProducerService } from './producer/ProducerService';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,9 +12,18 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
 
+  const config = new DocumentBuilder()
+    .setTitle('Sensors')
+    .setDescription('The sensors API description')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const producerService = app.get(ProducerService);
   await producerService.createGroupsAndSensors();
   await producerService.scheduleDataGeneration();
+
 
   await app.listen(3000);
 }
